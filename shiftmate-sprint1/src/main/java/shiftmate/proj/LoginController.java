ba2dc1cd153206597a742163866305b3b2a4eb98
Kellie Glasgow <kellie.glasgow16@gmail.com>
2024-03-07 19:25:40 -0600

    Sprint1

---
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/LoginController.java
@@ -0,0 +1,94 @@
+package shiftmate.proj;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.StageStyle;
+
+
+import java.io.IOException;
+
+
+public class LoginController
+{
+
+    @FXML
+    private Button loginButton;
+
+
+    @FXML
+    private Button closeButton;
+
+
+    @FXML
+    private Label loginMessageLabel;
+
+
+    @FXML
+    private TextField usernameTextfield;
+
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+
+
+
+    public void loginButtonOnAction(ActionEvent e) throws IOException
+    {
+
+
+        if (!usernameTextfield.getText().isBlank() && !passwordPasswordField.getText().isBlank())
+
+
+        {
+            if (usernameTextfield.getText().equals("ea") && passwordPasswordField.getText().equals("sp"))
+
+            {
+                loginMessageLabel.setText("Login Successful");
+                openMainPage();
+            }
+            else
+            {
+                loginMessageLabel.setText("Invalid Username/Password");
+            }
+        }
+        else
+        {
+            loginMessageLabel.setText("Input Username/Password");
+        }
+    }
+
+
+
+
+    public void closeButtonOnAction(ActionEvent e)
+    {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+
+
+    public void openMainPage() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root, 1250, 900));
+        stage.initStyle(StageStyle.DECORATED);
+        stage.show();
+
+
+    }
+
+
+}
---
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/Main.java
@@ -0,0 +1,36 @@
+package shiftmate.proj;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+import java.io.IOException;
+
+
+public class Main extends Application
+{
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.show();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
+
+
