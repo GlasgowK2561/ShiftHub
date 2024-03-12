ce326200c5579482183b106bcd5294f91637afb2
Kellie Glasgow <kellie.glasgow16@gmail.com>
2024-03-12 15:05:37 -0500

    Sprint2
    Added Login_Back to establish connection to the database

--- shiftmate-sprint1/src/main/java/shiftmate/proj/LoginController.java
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/LoginController.java
@@ -19,39 +19,21 @@ import java.io.IOException;
 
 public class LoginController
 {
-
     @FXML
     private Button loginButton;
-
-
     @FXML
     private Button closeButton;
-
-
     @FXML
     private Label loginMessageLabel;
-
-
     @FXML
     private TextField usernameTextfield;
-
-
     @FXML
     private PasswordField passwordPasswordField;
-
-
-
-
     public void loginButtonOnAction(ActionEvent e) throws IOException
     {
-
-
         if (!usernameTextfield.getText().isBlank() && !passwordPasswordField.getText().isBlank())
-
-
         {
             if (usernameTextfield.getText().equals("ea") && passwordPasswordField.getText().equals("sp"))
-
             {
                 loginMessageLabel.setText("Login Successful");
                 openMainPage();
@@ -66,18 +48,11 @@ public class LoginController
             loginMessageLabel.setText("Input Username/Password");
         }
     }
-
-
-
-
     public void closeButtonOnAction(ActionEvent e)
     {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
     }
-
-
-
 
     public void openMainPage() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
@@ -86,9 +61,5 @@ public class LoginController
         stage.setScene(new Scene(root, 1250, 900));
         stage.initStyle(StageStyle.DECORATED);
         stage.show();
-
-
     }
-
-
 }
--- shiftmate-sprint1/src/main/java/shiftmate/proj/MainController.java
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/MainController.java
@@ -8,71 +8,51 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
-
-
     @FXML
     private ImageView exit;
-
     @FXML
     private Label menu;
-
     @FXML
     private AnchorPane panel1, panel2;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         exit.setOnMouseClicked(mouseEvent -> {
             System.exit(0);
         });
-
-
         panel1.setVisible(false);
-
         // Set initial translation for panel2
         panel2.setTranslateX(-600);
-
         menu.setOnMouseClicked(event -> {
             if (panel1.isVisible()) {
-               
                 slideOutMenu();
             } else {
-
                 slideInMenu();
             }
         });
-
         panel1.setOnMouseClicked(event -> {
-        
             slideOutMenu();
         });
     }
-
     private void slideInMenu() {
         panel1.setVisible(true);
-
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), panel1);
         fadeTransition.setFromValue(0);
         fadeTransition.setToValue(1);
         fadeTransition.play();
-
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), panel2);
         translateTransition.setToX(0);
         translateTransition.play();
     }
-
     private void slideOutMenu() {
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), panel1);
         fadeTransition.setFromValue(1);
         fadeTransition.setToValue(0);
         fadeTransition.play();
-
         fadeTransition.setOnFinished(event -> panel1.setVisible(false));
-
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), panel2);
         translateTransition.setToX(-600);
         translateTransition.play();
