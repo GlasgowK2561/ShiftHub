ef318e093317f468d18a6c380fea9e295f6a0c34
Kellie Glasgow <kellie.glasgow16@gmail.com>
2024-03-21 18:36:38 -0500

    Added getEmployees function
    Added linked list to send data

--- shiftmate-sprint1/src/main/java/shiftmate/proj/Login_Back.java
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/Login_Back.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Login_Back{
     public static boolean login(String username, String password) {
         // Database connection parameters
@@ -45,6 +46,62 @@ public class Login_Back{
             // Handle any SQL exceptions
             e.printStackTrace();
             return(false);
+        }
+    }
+    public static List<String[]> getEmployees() {
+        // Database connection parameters
+        String url = "jdbc:mysql://localhost:3306/seniorproject_test";
+        String dbUsername = "shiftmate";
+        String dbPassword = "shift_testpass1";
+
+        // SQL query to retrieve all employees' information
+        String query = "SELECT * FROM employeeInfo";
+
+        try (
+            // Establishing a connection to the database
+            Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+            // Creating a prepared statement for the query
+            PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+            // Executing the query
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            // Create a list to store arrays representing each row of the result set
+            List<String[]> employees = new ArrayList<>();
+
+            // Iterating over the result set
+            while (resultSet.next()) {
+                // Retrieve data from each column
+                int employeeID = resultSet.getInt("employeeID");
+                String fName = resultSet.getString("fName");
+                String lName = resultSet.getString("lName");
+                String email = resultSet.getString("email");
+                String phone = resultSet.getString("phone");
+                String startDate = resultSet.getString("startDate");
+                String contact = resultSet.getString("contact");
+                String contactPhone = resultSet.getString("contactPhone");
+
+                // Create an array for the current row
+                String[] rowArray = {String.valueOf(employeeID), fName, lName, email, phone, startDate, contact, contactPhone};
+                
+                // Add the array representing the current row to the list
+                employees.add(rowArray);
+                // Print the current employee information
+                System.out.println("Employee ID: " + employeeID);
+                System.out.println("First Name: " + fName);
+                System.out.println("Last Name: " + lName);
+                System.out.println("Email: " + email);
+                System.out.println("Phone: " + phone);
+                System.out.println("Start Date: " + startDate);
+                System.out.println("Contact: " + contact);
+                System.out.println("Contact Phone: " + contactPhone);
+                System.out.println("----------------------");
+            }
+            return employees;
+        } catch (SQLException e) {
+            // Handle any SQL exceptions
+            e.printStackTrace();
+            return null;
         }
     }
 }
--- shiftmate-sprint1/src/main/java/shiftmate/proj/MainController.java
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/MainController.java
@@ -8,41 +8,30 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.List;
 
 
 public class MainController {
-
     @FXML
     private Button homeButton;
-
-
     @FXML
     private Button createScheduleButton;
-
-
     @FXML
     private Button editInformationButton;
-
-
     @FXML
     private Button staffButton;
-
-
     @FXML
     private Button DepartmentsButton;  
-
-
     @FXML  
     private Button logoutButton;
-
-
     @FXML
+
     private void homeButtonOnAction(ActionEvent e) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) homeButton.getScene().getWindow();
-       
         stage.setScene(new Scene(root, 1250, 800));
         stage.show();
     }
@@ -53,7 +42,6 @@ public class MainController {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createschedules.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) staffButton.getScene().getWindow();
-       
         stage.setScene(new Scene(root, 1250, 800));
         stage.show();
     }
@@ -64,23 +52,20 @@ public class MainController {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("editinformation.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) editInformationButton.getScene().getWindow();
-       
         stage.setScene(new Scene(root, 1250, 800));
         stage.show();
     }
-
 
     @FXML
     private void staffButtonOnAction(ActionEvent e) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("staff.fxml"));
+        List<String[]> employees = Login_Back.getEmployees();
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) staffButton.getScene().getWindow();
-       
         stage.setScene(new Scene(root, 1250, 800));
         stage.show();
     }
-
 
     @FXML
     private void departmentsButtonOnAction(ActionEvent e) throws IOException
@@ -88,7 +73,6 @@ public class MainController {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("departments.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) DepartmentsButton.getScene().getWindow();
-       
         stage.setScene(new Scene(root, 1250, 800));
         stage.show();
     }
@@ -99,6 +83,4 @@ public class MainController {
     {
        
     }
-
-
 }
