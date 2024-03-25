fe47b2f17651d196ed8b727aa3ea09d695c25050
njaae <elizabethnjaa@gmail.com>
2024-03-25 18:02:29 -0500

    Update DBController.java

--- shiftmate-sprint1/src/main/java/shiftmate/proj/DBController.java
+++ shiftmate-sprint1/src/main/java/shiftmate/proj/DBController.java
@@ -6,66 +6,88 @@ returning queried data to a hashtable is next
  package shiftmate.proj;
 
  import java.sql.*;
- import java.util.Dictionary;
- import java.util.Enumeration;
+ import java.util.*;
  import java.util.Hashtable;
  
  public class DBController {
-     // Database connection parameters
+    // Database connection parameters
          
-         static Connection connection;
-         static String url = "jdbc:mysql://dcm.uhcl.edu/sens24g2";
-         static String username = "sens24g2";
-         static String password = "Sce9902292!!";
+    static Connection connection;
+    static String url = "jdbc:mysql://dcm.uhcl.edu/sens24g2";
+    static String username = "sens24g2";
+    static String password = "Sce9902292!!";
  
-     public static void test(){
-             
-         try {
-             // Establishing a connection to the database
-             Connection connection = DriverManager.getConnection(url, username, password);
-             // If the connection is successful
-             System.out.println("Connected to the database.\n\n");
-             try {
-                 Statement stmt = connection.createStatement() ;
-                 try {
-                     String query = "SHOW TABLES" ; //replace with query
-                     ResultSet rs = stmt.executeQuery(query) ;
-                     try {
-                         while(rs.next()){ //iterates through returned data
-                             int numColumns = rs.getMetaData().getColumnCount();
-                             for(int i = 1; i <= numColumns; i++){
-                                 System.out.println(rs.getObject(i));
+    public static LinkedList<Hashtable<String,String>> test(String query){
+        LinkedList<Hashtable<String,String>> list = new LinkedList<>();     
+        
+        try {
+            // Establishing a connection to the database
+            Connection connection = DriverManager.getConnection(url, username, password);
+            // If the connection is successful
+            System.out.println("Connected to the database.\n\n");
+            try {
+                Statement stmt = connection.createStatement() ;
+                try {
+                    //String query = "SHOW TABLES" ; //replace with query
+                    ResultSet rs = stmt.executeQuery(query) ;
+                    try {
+                        /*while(rs.next()){ //iterates through returned data
+                            int numColumns = rs.getMetaData().getColumnCount();
+                            for(int i = 1; i <= numColumns; i++){
+                                System.out.println(rs.getObject(i));
                              
-                             }
-                         } 
-                     } finally {
-                         try { rs.close(); } 
-                         catch (Throwable ignore) { 
-                             // Propagate the original exception instead of this one that you may want just logged  
-                         }
-                         }
-                 } finally {
-                     try { stmt.close(); } 
-                     catch (Throwable ignore) { 
-                         // Propagate the original exception instead of this one that you may want just logged  
-                     }
-                 }
-             } finally {
-                 //It's important to close the connection when you are done with it
-                 try { connection.close(); } 
-                 catch (Throwable ignore) { 
-                     // Propagate the original exception instead of this one that you may want just logged  
-                 }
-             }
-         } catch (SQLException e) {   
-             // Handle any SQL exceptions
-             e.printStackTrace();
-         }
-     } //end of class
+                            }
+                        } 
+                        */
+                        while(rs.next()){
+                            Hashtable<String, String> currentRowMap = new Hashtable<>();
+                        
+                            ResultSetMetaData rsmd = rs.getMetaData();
+                            int columnCount = rsmd.getColumnCount();
+                            for (int i = 1; i <= columnCount; i++) {
+                                // retrieves column name and value.
+                                String key = rsmd.getColumnLabel(i);
+                                String value = rs.getString(rsmd.getColumnName(i));
+                                if (value == null) {
+                                    value = "null";
+                                }
+                                // builds map.
+                                currentRowMap .put(key, value);
+                            }
+                            list.add(currentRowMap);
+                        }
+                    } finally {
+                        try { rs.close(); } 
+                        catch (Throwable ignore) { 
+                        // Propagate the original exception instead of this one that you may want just logged  
+                        }
+                    }
+                    
+
+                } finally {
+                    try { stmt.close(); } 
+                    catch (Throwable ignore) { 
+                        // Propagate the original exception instead of this one that you may want just logged  
+                    }
+                }
+            } finally {
+                //It's important to close the connection when you are done with it
+                try { connection.close(); } 
+                catch (Throwable ignore) { 
+                    // Propagate the original exception instead of this one that you may want just logged  
+                }
+            }
+        } catch (SQLException e) {   
+            // Handle any SQL exceptions
+            e.printStackTrace();
+        }
+        return list;
+    } //end of class
      
-     public static void main (String[] args){
-         test();
+    public static void main (String[] args){
+        LinkedList<Hashtable<String,String>> resultlist = test("SHOW TABLES");
+        System.out.println(resultlist);
          
-     }
+    }
  }
  
